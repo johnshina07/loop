558cc87952f4888f70a7df5f07261b27c8d9228f
johnshina07 <johnshina2007@gmail.com>
2026-04-22 09:29:21 +0530

    first upload

---
+++ task13.js
@@ -0,0 +1,35 @@
+
+let number = 7;
+
+// Conditional Statements
+if (number > 0) {
+    console.log("The number is positive.");
+} else if (number < 0) {
+    console.log("The number is negative.");
+} else {
+    console.log("The number is zero.");
+}
+
+
+
+// 1. For Loop
+console.log("For Loop:");
+for (let i = 1; i <= 5; i++) {
+    console.log("i = " + i);
+}
+
+// 2. While Loop
+console.log("While Loop:");
+let j = 1;
+while (j <= 5) {
+    console.log("j = " + j);
+    j++;
+}
+
+// 3. Do-While Loop
+console.log("Do-While Loop:");
+let k = 1;
+do {
+    console.log("k = " + k);
+    k++;
+} while (k <= 5);
